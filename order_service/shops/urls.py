0652f6e027ceb3c24bc2156fb493b36376ec6be1
tsalair <toplider888@mail.ru>
2024-07-31 23:32:05 +0300

    Remove trailing slashes

--- order_service/shops/urls.py
+++ order_service/shops/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path("partner/update", views.PartnerUpdate.as_view()),
     path("partner/state", views.PartnerState.as_view()),
-    path('shops/', views.ShopList.as_view()),
-    path('products/', views.ProductInfoList.as_view()),
-    path('categories', views.CategoryList.as_view()),
+    path("shops", views.ShopList.as_view()),
+    path("products", views.ProductInfoList.as_view()),
+    path("categories", views.CategoryList.as_view()),
 ]
-
